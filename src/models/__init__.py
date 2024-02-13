ddeffb72dc93993b157e444cec33d3874be1b3d3
Pablo Munoz s310308 <s310308@studenti.polito.it>
2024-02-13 19:42:01 +0100

    Added LSTM classifier

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,3 +2,4 @@ from models.VideoModel import VideoModel
 from models.I3D import I3D
 from models.Original import OriginalClassifier
 from models.MLP import MLP_Classifier
+from models.LSTM import LSTM_Classifier
