a3746fdb5e885d8d5b97de72de5d03044543afe8
Pablo Munoz s310308 <s310308@studenti.polito.it>
2024-02-14 20:54:48 +0100

    Updated MLP Classifier removing avg pooling

--- epic-kitchens/models/MLP_classifier.py
+++ epic-kitchens/models/MLP_classifier.py
@@ -8,11 +8,9 @@ class MLP_Classifier(nn.Module):
         [TODO]: the classifier should be implemented by the students and different variations of it can be tested
         in order to understand which is the most performing one """
         self.model_config = model_config
-        self.avg_pool = nn.AvgPool1d(kernel_size=1, stride=1)
         self.dropout = nn.Dropout(self.model_config.dropout)
         self.linear = nn.Linear(1024, num_classes)
         self.classifier = nn.Sequential(
-            self.avg_pool,
             self.dropout,
             self.linear
         )
