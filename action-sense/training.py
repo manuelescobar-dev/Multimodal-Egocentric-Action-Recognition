6d57425ad9f518a836c07e5ab25e424d853dd33f
Manuel Escobar Ferrer <s307729@studenti.polito.it>
2024-02-17 23:47:07 +0100

    Added training pipeline for actionsense

--- action-sense/training.py
+++ action-sense/training.py
@@ -9,6 +9,7 @@ COMPLETED = {""}
 MODALITY = "EMG"
 CONFIG = f"configs/{MODALITY}_train.yaml"
 NAME = "emg_train"
+AUGMENTATION = ["10x1", "5x1", "10x20", "5x20"]
 
 
 def run_command(args):
@@ -21,6 +22,7 @@ def run_command(args):
         f"test.dense_sampling.{MODALITY}={args['sampling']}",
         f"train.stride.{MODALITY}={args['stride']}",
         f"test.stride.{MODALITY}={args['stride']}",
+        f"dataset.annotations_path={args['path']}",
     ]
     if args["resume_from"]:
         r = {"EMG": os.path.join("saved_models", args["name"])}
@@ -35,23 +37,25 @@ max_processes = 2  # Adjust as needed
 if __name__ == "__main__":
     logger.info("Starting training")
     arguments_list = []
-    for s in SAMPLING:
-        if s == True:
-            max_stride = 2
-        for i in range(1, max_stride + 1):
-            name = NAME + f"_{s}_{i}"
-            if name not in COMPLETED:
-                a = {
-                    "name": name,
-                    "sampling": s,
-                    "stride": i,
-                }
-                if os.path.exists(os.path.join("saved_models", name)):
-                    a["resume_from"] = True
-                    arguments_list.append(a)
-                else:
-                    a["resume_from"] = False
-                    arguments_list.append(a)
+    for a in AUGMENTATION:
+        for s in SAMPLING:
+            if s == True:
+                max_stride = 2
+            for i in range(1, max_stride + 1):
+                name = NAME + f"_{s}_{i}_{a}"
+                if name not in COMPLETED:
+                    a = {
+                        "name": name,
+                        "sampling": s,
+                        "stride": i,
+                        "path": f"data/final_{a}",
+                    }
+                    if os.path.exists(os.path.join("saved_models", name)):
+                        a["resume_from"] = True
+                        arguments_list.append(a)
+                    else:
+                        a["resume_from"] = False
+                        arguments_list.append(a)
 
     # Execute commands sequentially
     for args in arguments_list:
